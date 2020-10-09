5ab436d6401fded56a6da54b0d7c863a88469452
Gabriel Mendes <gabriel.mendes@dotlib.com.br>
2020-10-09 12:01:46 -0300

    bugfix: Mudado porta para 80, subi arquivo de deploy e deletei conf deploy antigo

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(routes);
 
-http.listen(3000, () => {
-  console.log('server is running on port', 3000);
+http.listen(80, () => {
+  console.log('server is running on port', 80);
 });
